b571184fa9f926eab8b475c2c9b4157723c3d826
Pramod Madushan <90685039+dopramo@users.noreply.github.com>
2025-06-20 10:29:40 +0530

    Update bot-script.js

--- bot-script.js
+++ bot-script.js
@@ -1,104 +1,229 @@
 var botui = new BotUI('help-bot');
 
+
 function startChat() {
   botui.message.add({
-    delay: 500,
+    delay: 300,
     loading: true,
-    content: 'Hi! Welcome to my website'
-  }).then(function () {
-    return botui.message.add({
-      delay: 500,
-      loading: true,
-      content: 'How can I help?'
-    });
+    content: 'Welcome, Admin! What would you like to do?'
   }).then(function () {
     return botui.action.button({
       action: [
-        {
-          text: 'What are your opening hours?',
-          value: 'hours'
-        },
-        {
-          text: 'What do you do?',
-          value: 'do'
-        }
+        { text: 'User Management', value: 'user_mgmt' },
+        { text: 'Chatbot Analytics', value: 'analytics' },
+        { text: 'FAQ Management', value: 'faq' },
+        { text: 'End Session', value: 'end' }
       ]
     });
-  }).then(handleMainResponse);
+  }).then(handleMainMenu);
 }
 
-function handleMainResponse(res) {
-  var message;
-
-  if (res.value === "hours") {
-    message = 'That’s a good one! This is a website, it’s always open.';
+function handleMainMenu(res) {
+  switch (res.value) {
+    case 'user_mgmt':
+      userManagement();
+      break;
+    case 'analytics':
+      showAnalytics();
+      break;
+    case 'faq':
+      faqManagement();
+      break;
+    case 'end':
+      endSession();
+      break;
   }
-  else if (res.value === "do") {
-    message = 'I’m a product-focused Scrum master<br><br>I also have a keen interest in chatbots and web analytics';
-  }
+}
 
+// --- User Management ---
+// Only keep instructions and menu
+function userManagement() {
   botui.message.add({
-    type: 'html',
-    delay: 1000,
-    loading: true,
-    content: message
-  }).then(function () {
-    return botui.action.button({
-      action: [
-        {
-          text: 'Cool!',
-          value: 'cool'
-        },
-        {
-          text: 'Go to Start',
-          value: 'start'
-        },
-        {
-          text: 'End Chat',
-          value: 'end'
-        }
-      ]
-    });
+    content: 'User Management:'
+  }).then(showUserActions);
+}
+
+function showUserActions() {
+  return botui.action.button({
+    action: [
+      { text: 'Add User', value: 'add' },
+      { text: 'Remove User', value: 'remove' },
+      { text: 'Check Profile', value: 'profile' },
+      { text: 'Back', value: 'back' }
+    ]
   }).then(function (res) {
-    if (res.value === 'start') {
-      botui.message.removeAll().then(startChat);
-    } else if (res.value === 'end') {
-      endChat();
-    } else {
-      botui.message.add({
-        delay: 1000,
-        loading: true,
-        content: 'I know! Thanks.'
-      }).then(function () {
-        return botui.action.button({
-          action: [
-            {
-              text: 'Start New Chat',
-              value: 'restart'
-            }
-          ]
-        });
-      }).then(function (res) {
-        if (res.value === 'restart') {
-          botui.message.removeAll().then(startChat);
-        }
-      });
+    switch (res.value) {
+      case 'add':
+        return showAddUserInstructions();
+      case 'remove':
+        return showRemoveUserInstructions();
+      case 'profile':
+        return showProfileInstructions();
+      case 'back':
+      default:
+        return botui.message.removeAll().then(startChat);
     }
   });
 }
 
-function endChat() {
+function showAddUserInstructions() {
+  return botui.message.add({
+    type: 'html',
+    content: `
+      <b>To add a user:</b><br>
+      1. Go to the admin dashboard.<br>
+      2. Select "Users" from the menu.<br>
+      3. Click "Add User" and fill in the details.<br>
+      <br>
+      <i>Contact IT support if you need further help.</i>
+    `
+  }).then(showUserActions);
+}
+
+function showRemoveUserInstructions() {
+  return botui.message.add({
+    type: 'html',
+    content: `
+      <b>To remove a user:</b><br>
+      1. Go to the admin dashboard.<br>
+      2. Select "Users" from the menu.<br>
+      3. Find the user and click "Remove".<br>
+      <br>
+      <i>Contact IT support if you need further help.</i>
+    `
+  }).then(showUserActions);
+}
+
+function showProfileInstructions() {
+  return botui.message.add({
+    type: 'html',
+    content: `
+      <b>To check a user profile:</b><br>
+      1. Go to the admin dashboard.<br>
+      2. Select "Users" from the menu.<br>
+      3. Click on the user's name to view their profile.<br>
+      <br>
+      <i>Contact IT support if you need further help.</i>
+    `
+  }).then(showUserActions);
+}
+
+// --- Analytics ---
+function showAnalytics() {
   botui.message.add({
-    delay: 500,
+    content: 'Chatbot Analytics:'
+  }).then(showAnalyticsActions);
+}
+
+function showAnalyticsActions() {
+  return botui.action.button({
+    action: [
+      { text: 'View Stats', value: 'stats' },
+      { text: 'Export Logs', value: 'export' },
+      { text: 'Back', value: 'back' }
+    ]
+  }).then(function (res) {
+    switch (res.value) {
+      case 'stats':
+        return botui.message.add({
+          type: 'html',
+          content: `
+            <b>To view chatbot statistics:</b><br>
+            1. Go to the analytics dashboard.<br>
+            2. Review total chats, common questions, and response times.<br>
+            <br>
+            <i>Contact IT support if you need further help.</i>
+          `
+        }).then(showAnalyticsActions);
+      case 'export':
+        return botui.message.add({
+          type: 'html',
+          content: `
+            <b>To export chat logs:</b><br>
+            1. Go to the analytics dashboard.<br>
+            2. Click "Export Logs" and choose your format.<br>
+            <br>
+            <i>Contact IT support if you need further help.</i>
+          `
+        }).then(showAnalyticsActions);
+      case 'back':
+      default:
+        return botui.message.removeAll().then(startChat);
+    }
+  });
+}
+
+// --- FAQ Management ---
+// Only keep instructions and menu
+function faqManagement() {
+  botui.message.add({
+    content: 'FAQ Management:'
+  }).then(showFaqActions);
+}
+
+function showFaqActions() {
+  return botui.action.button({
+    action: [
+      { text: 'Add FAQ', value: 'add' },
+      { text: 'Remove FAQ', value: 'remove' },
+      { text: 'Edit FAQ', value: 'edit' },
+      { text: 'Back', value: 'back' }
+    ]
+  }).then(function (res) {
+    switch (res.value) {
+      case 'add':
+        return botui.message.add({
+          type: 'html',
+          content: `
+            <b>To add an FAQ:</b><br>
+            1. Go to the admin dashboard.<br>
+            2. Select "FAQ Management".<br>
+            3. Click "Add FAQ" and enter the details.<br>
+            <br>
+            <i>Contact IT support if you need further help.</i>
+          `
+        }).then(showFaqActions);
+      case 'remove':
+        return botui.message.add({
+          type: 'html',
+          content: `
+            <b>To remove an FAQ:</b><br>
+            1. Go to the admin dashboard.<br>
+            2. Select "FAQ Management".<br>
+            3. Find the FAQ and click "Remove".<br>
+            <br>
+            <i>Contact IT support if you need further help.</i>
+          `
+        }).then(showFaqActions);
+      case 'edit':
+        return botui.message.add({
+          type: 'html',
+          content: `
+            <b>To edit an FAQ:</b><br>
+            1. Go to the admin dashboard.<br>
+            2. Select "FAQ Management".<br>
+            3. Find the FAQ and click "Edit".<br>
+            <br>
+            <i>Contact IT support if you need further help.</i>
+          `
+        }).then(showFaqActions);
+      case 'back':
+      default:
+        return botui.message.removeAll().then(startChat);
+    }
+  });
+}
+
+// --- End Session ---
+function endSession() {
+  botui.message.add({
+    delay: 400,
     loading: true,
-    content: 'Thank you for chatting! Have a great day.'
+    content: 'Session ended. Thank you!'
   }).then(function () {
     return botui.action.button({
       action: [
-        {
-          text: 'Start New Chat',
-          value: 'restart'
-        }
+        { text: 'Start New Chat', value: 'restart' }
       ]
     });
   }).then(function (res) {
